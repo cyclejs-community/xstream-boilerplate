549926db65e66b07714ff951df4411d6c701e222
Tylor Steinberger <tlsteinberger167@gmail.com>
2016-06-03 07:52:06 -0400

    test(e2e): hopefully fix timing issues on circleci

--- e2e/Landing.js
+++ e2e/Landing.js
@@ -8,14 +8,14 @@ describe('Landing page', () => {
   it('should show a welcoming', (browser) => {
     browser
       .url(URL)
-      .waitForElementVisible('body', 3000)
-      .pause(2000)
+      .waitForElementVisible('body', 5000)
+      .pause(3000)
       .assert.containsText('.welcome', 'Cycle.js Diversity XStream Boilerplate')
   })
 
   it('should route to the Login page after 3 seconds', (browser) => {
-    browser.pause(3100)
-      .assert.containsText('.login', 'Login Page')
+    browser.pause(4000)
+      .assert.containsText('.login', 'Login page')
       .end()
   })
 })

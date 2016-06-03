38f7aaf02670f93196c4ebdd5b23791667d03b7a
Tylor Steinberger <tlsteinberger167@gmail.com>
2016-06-03 16:51:29 -0400

    chore(test): circleci fixes

--- test/component/ComponentRouter.js
+++ test/component/ComponentRouter.js
@@ -62,8 +62,6 @@ describe('ComponentRouter', () => {
 
     const DOM = pluck('DOM')
 
-    assert(typeof DOM.addListener === 'function')
-
     // Test Loading page
     DOM.take(1).addListener({
       next: vNode => {

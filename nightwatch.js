e73ab0d932eda6aa39cdd73fdbb9783366a336dd
Tylor Steinberger <tlsteinberger167@gmail.com>
2016-06-03 17:48:05 -0400

    chore(misc): remove CBT

--- nightwatch.js
+++ nightwatch.js
@@ -1,29 +1,3 @@
-const CBT_USERNAME = process.env.CBT_USERNAME
-const CBT_PASSWORD = process.env.CBT_PASSWORD
-const START_PROCESS = process.env.E2E !== 'CBT'
-
-const makeCBTTest = OSName => (apiName, browserName) =>
-  ({
-    'selenium_host': 'hub.crossbrowsertesting.com',
-    'selenium_port': '80',
-
-    desiredCapabilities: {
-      'name': `${OSName}~${apiName}~${browserName}`,
-      'browser_api_name': apiName,
-      'os_api_name': OSName,
-      'browserName': browserName.toLowerCase(),
-      'record_video': 'true',
-      'record_network': 'true',
-      'username': CBT_USERNAME,
-      'password': CBT_PASSWORD,
-      'access_key': CBT_PASSWORD
-    }
-  })
-
-const makeWin8 = makeCBTTest('Win8.1')
-const makeWin10 = makeCBTTest('Win10')
-const makeMac = (n, api) => makeCBTTest(`Mac10.${n}`)(api, 'safari')
-
 module.exports = {
   'src_folders': ['e2e'],
   'output_folder': './report',
@@ -33,7 +7,7 @@ module.exports = {
   'globals_path': '',
 
   'selenium': {
-    'start_process': START_PROCESS,
+    'start_process': true,
     'server_path': 'node_modules/selenium-server/lib/runner/selenium-server-standalone-2.53.0.jar',
     'log_path': '',
     'host': '127.0.0.1',
@@ -50,7 +24,7 @@ module.exports = {
 
   'test_settings': {
     'default': {
-      'launch_url': 'http://local',
+      'launch_url': 'http://localhost:8080',
       'selenium_port': 4444,
       'selenium_host': 'localhost',
       'output_folder': './report',
@@ -73,24 +47,6 @@ module.exports = {
         javascriptEnabled: true,
         acceptSslCerts: true
       }
-    },
-
-    'WIN7-IE9': makeCBTTest('Win7x64-C1')('IE9', 'internet explorer'),
-    'WIN7-IE10': makeCBTTest('Win7x64-C2')('IE10', 'internet explorer'),
-    'WIN7-IE11': makeCBTTest('Win7x64-BASE')('IE11', 'internet explorer'),
-    'WIN8-IE11': makeWin8('IE11', 'internet explorer'),
-    'WIN10-IE11': makeWin10('IE11', 'internet explorer'),
-    'WIN10-Chrome49': makeWin10('Chrome49x64', 'chrome'),
-    'MAC9': makeMac('9', 'Safari7'),
-    'MAC10': makeMac('10', 'Safari8'),
-    'MAC11': makeMac('11', 'Safari9'),
-    'Android42': makeCBTTest('Nexus7-And42')('MblChrome37', 'chrome'),
-    'Android44': makeCBTTest('GalaxyS5-And44')('MblChrome35', 'chrome'),
-    'iPadPro93': makeCBTTest('iPadPro-iOS9Sim')('MblSafari9.0', 'safari'),
-    'iPadAir8': makeCBTTest('iPadAir-iOS8Sim')('MblSafari8.0', 'safari'),
-    'iPadMini': makeCBTTest('iPadMiniRetina-iOS7Sim')('MblSafari7.0', 'safari'),
-    'iPhone6P': makeCBTTest('iPhone6Plus-iOS8sim')('MblSafari8.0', 'safari'),
-    'iPhone6': makeCBTTest('iPhone6-iOS8sim')('MblSafari8.0', 'safari'),
-    'iPhone5s': makeCBTTest('iPhone5s-iOS7sim')('MblSafari7.0', 'safari')
+    }
   }
 }

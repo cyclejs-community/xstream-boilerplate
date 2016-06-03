4ba789977c7c1f0c4725775b6e36f686c590f326
Tylor Steinberger <tlsteinberger167@gmail.com>
2016-06-03 07:46:59 -0400

    feat(Login): add Login page and update to latest dependencies
    Updates xstream to 4.0.0 and related packages
    
    xstream had a large breaking change

--- e2e/Landing.js
+++ e2e/Landing.js
@@ -11,6 +11,11 @@ describe('Landing page', () => {
       .waitForElementVisible('body', 3000)
       .pause(2000)
       .assert.containsText('.welcome', 'Cycle.js Diversity XStream Boilerplate')
+  })
+
+  it('should route to the Login page after 3 seconds', (browser) => {
+    browser.pause(3100)
+      .assert.containsText('.login', 'Login Page')
       .end()
   })
 })
--- src/page/main.js
+++ src/page/main.js
@@ -5,15 +5,22 @@ import xs from 'xstream'
 
 // Pages
 import Landing from './Landing'
+import Login from './Login'
 
 // Components
 import ComponentRouter from 'component/ComponentRouter'
 
 const routes =
   {
-    '/': Landing
+    '/': Landing,
+    '/login': Login
   }
 
 export default function main (sources) {
-  return ComponentRouter({...sources, routes$: xs.of(routes)})
+  const page = ComponentRouter({...sources, routes$: xs.of(routes)})
+
+  return {
+    DOM: page.DOM,
+    router: page.route$
+  }
 }

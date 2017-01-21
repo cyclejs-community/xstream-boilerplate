1473c5bc453906057a9e7615f2c7269e6a8b14c4
Churchill Lee <churchill@piktochart.com>
2017-01-21 17:08:21 +0800

    test(e2e): remove e2e test

--- nightwatch.conf.js
+++
@@ -1,9 +0,0 @@
-require('babel-register')
-
-module.exports = (function (settings) {
-  if (process.platform === 'win32') {
-    settings.selenium.cli_args['webdriver.chrome.driver'] =
-      './node_modules/.bin/chromedriver.cmd'
-  }
-  return settings
-})(require('./nightwatch.js'))
